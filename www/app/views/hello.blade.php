802625cbdcd695bb80beb159502cdfdbe0ab32f8
chromelife <chromelifeinc@gmail.com>
2014-07-30 17:14:42 +0100

    foo

--- www/app/views/hello.blade.php
+++ www/app/views/hello.blade.php
@@ -5,7 +5,7 @@
 		<section class="img-dragger img-dragger-large dragdealer">
 			<div class="handle">
 				@foreach ($images as $image)
-					<div class="slide" data-content="content-{{$image->id}}">
+					<div class="slide" data-content="content-{{$image->title}}">
 					<div class="img-wrap"><img src="{{$image->image_url}}" alt=""/></div>
 					<h2>Title Not Working Yet<span>Cause I didn't account for it really</span></h2>
 					<button class="content-switch">Read more</button>
